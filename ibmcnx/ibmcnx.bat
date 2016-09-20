36bc5df23c99d8cc6b4bf1659e5f1c63a194566f
thardenb <thardenb@gmail.com>
2016-09-20 12:27:54 -0700

    Create batch file for Windows

--- ibmcnx/ibmcnx.bat
+++ ibmcnx/ibmcnx.bat
@@ -1,1 +1,7 @@
+REM Replace JDBC_DRIVER value with the correct location of the JDBC driver
+REM Update values for -username and -password 
+
+SET JDBC_DRIVER=D:\IBM\SQLLIB\java\db2jcc4.jar
+
+wsadmin.bat -lang jython -username wasadmin -password password -javaoption -Dcom.ibm.ws.scripting.classpath=%JDBC_DRIVER% -f ibmcnx/menu/cnxMenu.py
 
